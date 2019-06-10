2b3ba5a4f9aca95ec6c54e72f9f5c808ff735aee
nkinkade <kinkade@measurementlab.net>
2019-06-10 14:55:14 -0600

    Removes measurement-lab.org zone (#39)
    * Removes measurement-lab.org zone and zone generation script, as the zone is now created and published by the siteinfo repo.
    
    * Removes the operator submodue, as it is no longer needed since measurement-lab.org zone is now created and published  by the siteinfo repo.
    
    * Removes the mlab.fun zone. It was never used and we don't even own the domain any longer.
    
    * Updates README to reflect how the measurement-lab.org zone is handled these days.

--- gen-zone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Ultra simple wrapper to 'mlabconfig.py --format=zone'
-
-MLABCONFIG='operator/plsync/mlabconfig.py'
-ZONE_HEADER='operator/plsync/mlabzone.header.in'
-OUTPUT_FILE='measurement-lab.org'
-
-$MLABCONFIG --format=zone --zoneheader=$ZONE_HEADER > $OUTPUT_FILE
